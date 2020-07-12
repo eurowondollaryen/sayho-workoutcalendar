1c8eaccc8672592c70fe44429b800f9e7242cf47
Sayho <sehoakasayho@naver.com>
2020-07-12 22:07:24 +0900

    first commit

---
+++ src/main/java/com/sayho/workoutcalendar/SayhoWorkoutcalendarApplication.java
@@ -0,0 +1,13 @@
+package com.sayho.workoutcalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SayhoWorkoutcalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SayhoWorkoutcalendarApplication.class, args);
+	}
+
+}
