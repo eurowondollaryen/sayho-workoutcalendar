6e5ac583cfae0ec7c985171b3c28589990aa8a00
Sayho <sehoakasayho@naver.com>
2020-07-16 00:00:08 +0900

    added workout add function and getting workout info from db-heroku

--- src/main/java/com/sayho/workoutcalendar/model/Workout.java
+++ src/main/java/com/sayho/workoutcalendar/model/Workout.java
@@ -2,13 +2,44 @@ package com.sayho.workoutcalendar.model;
 
 public class Workout {
 	private String woDiv;
+	private String partMainCod;
+	private String partMainNm;
+	private String partSubCod;
+	private String partSubNm;
 	private String woCod;
-	private String woNm;
+	private String woNmEn;
+	private String woNmKor;
+	private String woNmJp;
+	private float weightRate;
 	public String getWoDiv() {
 		return woDiv;
 	}
 	public void setWoDiv(String woDiv) {
 		this.woDiv = woDiv;
+	}
+	public String getPartMainCod() {
+		return partMainCod;
+	}
+	public void setPartMainCod(String partMainCod) {
+		this.partMainCod = partMainCod;
+	}
+	public String getPartMainNm() {
+		return partMainNm;
+	}
+	public void setPartMainNm(String partMainNm) {
+		this.partMainNm = partMainNm;
+	}
+	public String getPartSubCod() {
+		return partSubCod;
+	}
+	public void setPartSubCod(String partSubCod) {
+		this.partSubCod = partSubCod;
+	}
+	public String getPartSubNm() {
+		return partSubNm;
+	}
+	public void setPartSubNm(String partSubNm) {
+		this.partSubNm = partSubNm;
 	}
 	public String getWoCod() {
 		return woCod;
@@ -16,10 +47,28 @@ public class Workout {
 	public void setWoCod(String woCod) {
 		this.woCod = woCod;
 	}
-	public String getWoNm() {
-		return woNm;
+	public String getWoNmEn() {
+		return woNmEn;
 	}
-	public void setWoNm(String woNm) {
-		this.woNm = woNm;
+	public void setWoNmEn(String woNmEn) {
+		this.woNmEn = woNmEn;
+	}
+	public String getWoNmKor() {
+		return woNmKor;
+	}
+	public void setWoNmKor(String woNmKor) {
+		this.woNmKor = woNmKor;
+	}
+	public String getWoNmJp() {
+		return woNmJp;
+	}
+	public void setWoNmJp(String woNmJp) {
+		this.woNmJp = woNmJp;
+	}
+	public float getWeightRate() {
+		return weightRate;
+	}
+	public void setWeightRate(float weightRate) {
+		this.weightRate = weightRate;
 	}
 }
